ec2573533f6c39f66abd782dc73ed54f0c9fd421
nburkard <nburkard@rhrk.uni-kl.de>
2020-01-31 22:12:50 +0100

    pinguini

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody>().velocity = new Vector3(move * maxSpeed, 0f, 0f);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Boundary"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
