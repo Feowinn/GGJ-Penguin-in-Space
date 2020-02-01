6be9e5d28228e7fe10c2d8b7f23e1292d59a9f54
nburkard <nburkard@rhrk.uni-kl.de>
2020-02-01 12:39:12 +0100

    more logic!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public MeteorSpawnScript meteorSpawnScript;
     public float maxSpeed = 5f;
-    //public ShipPart[] parts = new ShipPart[5];
+
     public ShipPart[] shipParts = new ShipPart[5];
     //public ShipPart dome, leftFront, leftBack, rightFront, right
     bool[] broken = new bool[5] {false,false,false,false,false };
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,15 +33,41 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Meteor"))
         {
             GameObject part = collision.contacts[0].thisCollider.gameObject;
+            // despawn Meteor
+            meteorSpawnScript.AddDeactivatedMeteor(collision.gameObject);
+
             //only damage ship parts
             if (!part.Equals(this.gameObject))
             {
-            broken[part.GetComponent<ShipPart>().partNumber] = true;
-            part.SetActive(false);
-            //part.GetComponent<ShipPart>()
+                broken[part.GetComponent<ShipPart>().partNumber] = true;
+                part.SetActive(false);
 
             }
 
+            // do something when all parts are broken
+            if (part.Equals(this.gameObject))
+            {
+                for(int i = 4; i >=0; i--)
+                {
+                    if (!broken[i])
+                    {
+                        broken[i] = true;
+                        shipParts[i].gameObject.SetActive(false);
+                        break;
+                    }
+                    if (i == 0)
+                    {
+                        // TODO lose the game!
+                    }
+                }
+                
+            }
+        }
+
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            meteorSpawnScript.AddDeactivatedCollectible(collision.gameObject);
+            //TODO add method 
         }
 
     }
@@ -53,7 +81,19 @@ public class PlayerController : MonoBehaviour
     public void ShipPartIsRepaired(int shipNumber)
     {
         broken[shipNumber] = false;
-        //parts[shipNumber].enabled = true;
         shipParts[shipNumber].gameObject.SetActive(true);
+    }
+
+    public void RepairAnyPart()
+    {
+        for( int i = 0; i < shipParts.Length; i++)
+        {
+            if (broken[i])
+            {
+                shipParts[i].gameObject.SetActive(true);
+                broken[i] = false;
+                break;
+            }
+        }
     }
 }
